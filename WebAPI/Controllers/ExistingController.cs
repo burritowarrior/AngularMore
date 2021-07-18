0e906bedd32f014c67e37c506748de4f48770b63
Christopher Oson <coson16@gmail.com>
2021-07-17 17:33:32 -0700

    Removed old code

--- WebAPI/Controllers/ExistingController.cs
+++ WebAPI/Controllers/ExistingController.cs
@@ -16,15 +16,6 @@ namespace WebAPI.Controllers
             // return BadRequest("An error has occurred...");
 
             // _log.Information("Inside rate retrieval");
-
-            // using var sqlConn = new SqlConnection(_connectionString);
-            // sqlConn.Open();
-            // var rates = sqlConn.Query<BaseRate>($"SELECT BaseRateId, LotNumber, ParkingStallType, " +
-            //                                     "RateNumber, [Description], StartDate, EndDate, Amount, ChildBaseRateId, " +
-            //                                     "Username, DateEntered, IsAvailable, RateType, " +
-            //                                     "IsPortalRate, MarketRateId, IsCleaned, NewRateNumber, " +
-            //                                     $"IsRowLocked FROM [MRP].[vw_Rates] WHERE LotNumber = '{lotNumber}'").ToList();
-
             var dataLayer = new DAL.DataLayer();
             var rates = dataLayer.GetBaseRates(lotNumber);
 
