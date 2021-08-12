73c3f01264b7ef8b2f222a8e3e6a4b3f220e2103
Christopher Oson <coson16@gmail.com>
2021-08-11 20:49:33 -0700

    Renames BaseRate to SimpleRate

--- WebAPI/Controllers/SimpleRateController.cs
+++ WebAPI/Controllers/SimpleRateController.cs
@@ -7,7 +7,7 @@ namespace WebAPI.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class BaseRateController : ControllerBase
+    public class SimpleRateController : ControllerBase
     {
         [HttpPost]
         [Route("InsertData")]
